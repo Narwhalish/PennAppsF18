7afd7ecc22e95860dc2c941e65de1463afb8c41d
ivwang46 <ivy.wang121101@gmail.com>
2018-09-08 17:27:30 -0400

    GREEN

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import sys, pygame
+pygame.init()
 
-size = width, height = 700, 700
-screen = pygame.display.set_mode(size)
-green = [0, 255, 0]
-screen.fill(green)
+def main():
+    #set up screen, make green
+    size = width, height = 700, 700
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption("Level Up!")
+    green = [0,255,0]
+    screen.fill(green)
+    pygame.display.update()
