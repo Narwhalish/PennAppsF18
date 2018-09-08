dad7303b3a195b2d58f5c618886961e95478080a
Narwhalish <bryao@ctemc.org>
2018-09-08 08:50:13 -0400

    Attempt to consolidate files in test.py

---
+++ test.py
@@ -0,0 +1,44 @@
+#* /usr/bin/python3
+# -*- encoding: utf-8 -*-
+
+import pygame
+import os
+import sys
+import random
+
+class Card:
+    def __init__(self, card_code):
+        self.rank = card_code[0]
+        self.suit = card_code[1]
+
+    def get_rank(self):
+        return self.rank
+
+    def get_suit(self):
+        return self.suit
+
+    def get_value(self):
+        value_cards = {'5': 5, '10': 10, 'K': 10}
+        return value_cards[self.rank]
+
+class Deck:
+    card_ranks = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    card_suits = ['C', 'D', 'H', 'S']
+
+    def __init__(self):
+        self.deck = [rank + suit for rank in self.card_ranks for suit in self.card_suits]
+
+    def shuffle_deck(self):
+        random.shuffle(self.deck)
+
+    #temp method
+    def print_deck(self):
+        print(self.deck)
+
+class Game:
+
+
+if __name__ == '__main__':
+    d1 = Deck()
+    d1.shuffle_deck()
+    d1.print_deck()
